0fbcdb427debef2c61a774b976e21977930f6a63
Gibettt <145196968+Gibettt@users.noreply.github.com>
2025-09-15 09:09:21 +0700

    1.1

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,117 @@
 import Image from "next/image";
+import Lanyard from "./components/Lanyard/Lanyard";
+import RotatingText from "./components/RotatingText/RotatingText";
+import SplitText from "./components/SplitText/SplitText";
+import BlurText from "./components/BlurText/BlurText";
+import AnimatedContent from "./components/AnimatedContent/AnimatedContent";
+import Squares from "./components/Squares/Squares";
+import SplashCursor from "./components/SplashCursor/SplashCursor";
+import ScrollVelocity from "@/components/ScrollVelocity";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="min-h-screen overflow-x-hidden bg-[#19222D]">
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+      {/* ini cursor*/}
+      <SplashCursor />
+
+      <div className="absolute top-0 right-0 left-0 left-0 bottom-0 w-full h-full">
+      <Squares 
+      speed={0.5} 
+      squareSize={50}
+      direction='diagonal' // up, down, left, right, diagonal
+      borderColor='#F1FFB2'
+      hoverFillColor='#C6F10E'
+      />
+      </div>
+      <div className="w-full h-screen">
+        <div className="grid grid-cols-12 h-full">
+
+          {/* Bagian teks kiri */}
+          <div className="col-span-6 flex flex-col justify-center gap-6 pl-4">
+  
+  {/* Judul kecil + rotating text */}
+  <AnimatedContent
+    distance={150}
+    direction="horizontal"
+    reverse={false}
+    duration={1.2}
+    ease="bounce.out"
+    initialOpacity={0.2}
+    animateOpacity
+    scale={1.1}
+    threshold={0.2}
+    delay={0.3}
+  >
+    <h1 className="text-2xl text-white font-bold">I'm Ready For Job</h1>
+    <RotatingText
+      texts={[
+        "Web Development",
+        "Web Design",
+        "Web Programing",
+        "Ai Development",
+        "Akuntansi",
+      ]}
+      mainClassName="px-2 sm:px-2 md:px-3 bg-[#A0CF26] text-black overflow-hidden py-0.5 sm:py-1 justify-center rounded-lg text-2xl font-bold inline-flex transition-all"
+      staggerFrom={"last"}
+      initial={{ y: "100%" }}
+      animate={{ y: 0 }}
+      exit={{ y: "-120%" }}
+      staggerDuration={0.025}
+      splitLevelClassName="overflow-hidden pb-0.5 sm:pb-1 md:pb-1"
+      transition={{ type: "spring", damping: 30, stiffness: 400 }}
+      rotationInterval={2000}
+    />
+  </AnimatedContent>
+
+  {/* Nama & posisi */}
+  <div className="flex flex-col items-start gap-2">
+    <SplitText
+      text="I'm Fransico Gilbert"
+      className="text-6xl font-semibold text-left-[#A0CF26] text-white"
+      delay={150}
+      ease="power3.out"
+      splitType="chars"
+      from={{ opacity: 0, transform: "translate3d(0,50px,0)" }}
+      to={{ opacity: 1, transform: "translate3d(0,0,0)" }}
+      threshold={0.2}
+      rootMargin="-50px"
+    />
+    <SplitText
+      text="Full Stack Developer"
+      className="text-6xl font-semibold text-left text-[#A0CF26]"
+      delay={75}
+      ease="power3.out"
+      splitType="chars"
+      from={{ opacity: 0, transform: "translate3d(0,50px,0)" }}
+      to={{ opacity: 1, transform: "translate3d(0,0,0)" }}
+      threshold={0.2}
+      rootMargin="-50px"
+    />
+    </div>
+
+    {/* Deskripsi */}
+    <BlurText
+      text="A highly motivated student of Accounting Information Systems with a strong passion for combining financial accuracy and modern technology. Proficient in financial reporting, accounting software, data analysis, and full-stack web development. Experienced in designing and building web-based financial systems using tools such as Accurate, Zahir, MySQL, HTML, CSS, PHP, and Laravel. Eager to contribute innovative digital solutions in the field of accounting and finance."
+      delay={50}
+      animateBy="words"
+      direction="top"
+      className="text-xl text-white max-w-2xl"
+      />
+    </div>
+
+
+          {/* Bagian kanan */}
+          <div className="col-span-6 flex items-center justify-center">
+            <Lanyard position={[0, 0, 12]} gravity={[0, -40, 0]} />
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className="w-full min-h-screen text-[#A0CF26] text-white">
+        <ScrollVelocity
+          texts={['Saya Fransisco Gilbert', 'Mari Kita Belajar Bareng Tentang React']}
+        />
+        </div>
+      </div>
     </div>
   );
 }
